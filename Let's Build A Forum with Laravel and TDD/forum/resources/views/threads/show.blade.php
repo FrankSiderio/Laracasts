6d1e023951ff17d26850a46eff7e7395e027c114
Bcouden <frank.siderio1@marist.edu>
2018-06-08 16:27:00 -0400

    episode 16

--- Let's Build A Forum with Laravel and TDD/forum/resources/views/threads/show.blade.php
+++ Let's Build A Forum with Laravel and TDD/forum/resources/views/threads/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
@@ -14,25 +14,16 @@
                     {{ $thread->body }}
                 </div>
             </div>
-        </div>
-    </div>
+            <br>
 
-    <br>
-
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @foreach($thread->replies as $reply)
+            @foreach($replies as $reply)
                 @include('threads.reply')
                 <br>
             @endforeach
-        </div>
-    </div>
 
-    <br>
+            {{ $replies->links() }}
 
-    @if(auth()->check())
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+            @if(auth()->check())
                 <form method="POST" action="{{ $thread->path() . '/replies' }}">
                     <div class="form-group">
                         {{ csrf_field() }}
@@ -44,10 +35,22 @@
                         <button type="submit" class="btn btn-primary">Post</button>
                     </div>
                 </form>
+            @else
+                <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+            @endif
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <p>
+                        This thread was published {{ $thread->created_at->diffForHumans() }} by
+                        <a href="#">{{ $thread->creator->name }}</a> and currently 
+                        has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                    </p>
+                </div>
             </div>
         </div>
-    @else
-        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-    @endif
+    </div>
 </div>
 @endsection
