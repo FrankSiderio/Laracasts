f0c20ab4f1dd5b09447d6da0d8eff2574b20fac1
Bcouden <frank.siderio1@marist.edu>
2018-06-11 23:46:04 -0400

    adding favorites button

--- Let's Build A Forum with Laravel and TDD/forum/resources/views/threads/reply.blade.php
+++ Let's Build A Forum with Laravel and TDD/forum/resources/views/threads/reply.blade.php
@@ -1,9 +1,24 @@
 <div class="card">
     <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a>
-        said {{ $reply->created_at->diffForHumans() }}...
+    	<div class="level">
+    		<h6 class="flex">
+		        <a href="#">
+		            {{ $reply->owner->name }}
+		        </a>
+		        said {{ $reply->created_at->diffForHumans() }}...
+	    	</h6>
+
+	        <div>
+
+	        	<form method="POST" action="/replies/{{ $reply->id }}/favorites">
+	        		{{ csrf_field() }}
+
+	        		<button type="submit" class="btn btn-primary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+	        			{{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+	        		</button>
+	        	</form>
+	        </div>
+    	</div>
     </div>
 
     <div class="card-body">
