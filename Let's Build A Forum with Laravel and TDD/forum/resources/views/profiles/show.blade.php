73f75d200c3b5bb181948b084d380c63a1ea6a85
Bcouden <frank.siderio1@marist.edu>
2018-06-18 12:30:35 -0400

    activity feed part 2

--- Let's Build A Forum with Laravel and TDD/forum/resources/views/profiles/show.blade.php
+++ Let's Build A Forum with Laravel and TDD/forum/resources/views/profiles/show.blade.php
@@ -7,31 +7,18 @@
 				<div class="pb-2 mt-4 mb-4 border-bottom">
 					<h1>
 						{{ $profileUser->name }}
-						<small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
 					</h1>
 
 				</div>
 
-				@foreach($threads as $thread)
-					<div class="card mb-4">
-		                <div class="card-header">
-		                	<div class="level">
-		                		<span class="flex">
-		                    		<a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted:
-		                    		<a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-		                    	</span>
-
-		                    	<span>{{ $thread->created_at->diffForHumans() }}</span>
-		                    </div>
-		                </div>
-
-		                <div class="card-body">
-		                    {{ $thread->body }}
-		                </div>
-		            </div>
+				@foreach($activities as $date => $activity)
+					<h3 class="pb-2 mt-4 mb-4 border-bottom">{{ $date }}</h3>
+					@foreach($activity as $record)
+						@include("profiles.activities.{$record->type}", ['activity' => $record])
+					@endforeach
 				@endforeach
 
-				{{ $threads->links() }}
+				
 
 			</div>
 		</div>
