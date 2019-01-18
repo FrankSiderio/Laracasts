fd5e94555d4adeeed0878cc871b4a05fdd22545f
FrankSiderio <frank_siderio@comcast.com>
2019-01-18 12:51:10 -0500

    episode 16 adding notes and policies

--- birdboard/app/Http/Controllers/ProjectTasksController.php
+++ birdboard/app/Http/Controllers/ProjectTasksController.php
@@ -4,15 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Project;
 use App\Task;
-use Illuminate\Http\Request;
 
 class ProjectTasksController extends Controller
 {
     public function store(Project $project)
     {
-        if(auth()->user()->isNot($project->owner)) {
-            abort(403);
-        }
+        $this->authorize('update', $project);
         
         request()->validate(['body' => 'required']);
 
@@ -23,9 +20,7 @@ class ProjectTasksController extends Controller
 
     public function update(Project $project, Task $task)
     {
-        if(auth()->user()->isNot($project->owner)) {
-            abort(403);
-        }
+        $this->authorize('update', $project);
 
         request()->validate(['body' => 'required']);
 
