1ff5eac8c7e3c6c731eeb7b8232e9391759fa984
FrankSiderio <frank_siderio@comcast.com>
2019-01-28 10:34:36 -0500

    episode 21 activities fun time

--- birdboard/tests/Unit/TaskTest.php
+++ birdboard/tests/Unit/TaskTest.php
@@ -18,12 +18,25 @@ class TaskTest extends TestCase
 
         $this->assertInstanceOf(Project::class, $task->project);
     }
+
     /** @test */
     public function it_has_a_path()
     {
         $task = factory(Task::class)->create();
 
         $this->assertEquals('/projects/' . $task->project->id . '/tasks/' . $task->id, $task->path());
+    }
+
+    /** @test */
+    public function it_can_be_completed()
+    {
+        $task = factory(Task::class)->create();
+
+        $this->assertFalse($task->completed);
+
+        $task->complete();
+
+        $this->assertTrue($task->fresh()->completed);
     }
 
 
