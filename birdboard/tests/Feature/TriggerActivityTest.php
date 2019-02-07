715f97760c597c498161c09e511e5f93d800e4d5
FrankSiderio <frank_siderio@comcast.com>
2019-02-07 09:39:08 -0500

    episode 25 including more details in activity feeds

--- birdboard/tests/Feature/TriggerActivityTest.php
+++ birdboard/tests/Feature/TriggerActivityTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Task;
 use Facades\Tests\Setup\ProjectFactory;
+use function foo\func;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -38,7 +40,12 @@ class TriggerActivityTest extends TestCase
         $project->addTask('Some task');
 
         $this->assertCount(2, $project->activity);
-        $this->assertEquals('created_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function($activity) {
+            $this->assertEquals('created_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+            $this->assertEquals('Some task', $activity->subject->body);
+        });
     }
 
     /** @test */
@@ -53,7 +60,11 @@ class TriggerActivityTest extends TestCase
         ]);
 
         $this->assertCount(3, $project->activity);
-        $this->assertEquals('completed_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function($activity) {
+            $this->assertEquals('completed_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+        });
     }
 
     /** @test */
