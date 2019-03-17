f18186308ac25a26d8e06a4fd33dd65c453a132e
FrankSiderio <frank_siderio@comcast.com>
2019-03-17 15:13:03 -0400

    episode 30 inviting users

--- birdboard/app/Project.php
+++ birdboard/app/Project.php
@@ -35,13 +35,13 @@ class Project extends Model
         return $this->hasMany(Activity::class)->latest();
     }
 
-//    public function invite(User $user)
-//    {
-//        return $this->members()->attach($user);
-//    }
-//
-//    public function members()
-//    {
-//        return $this->belongsToMany(User::class, 'project_members');
-//    }
+    public function invite(User $user)
+    {
+        return $this->members()->attach($user);
+    }
+
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'project_members');
+    }
 }
