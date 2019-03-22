1c34d6a7cc55e383935ded3e46480a3447da0f8c
FrankSiderio <frank_siderio@comcast.com>
2019-03-22 16:42:24 -0400

    episode 35 invited users may not delete projects

--- birdboard/database/factories/ProjectFactory.php
+++ birdboard/database/factories/ProjectFactory.php
@@ -6,6 +6,7 @@ $factory->define(App\Project::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence,
         'description' => $faker->text(100), // The limit that the project card will show
+        'notes' => 'Foo bar notes',
         'owner_id' => factory(App\User::class)
     ];
 });
