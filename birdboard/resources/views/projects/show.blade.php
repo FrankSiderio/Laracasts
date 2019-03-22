50ac24053ed3f703dced9d5e863ac259b9ed7019
FrankSiderio <frank_siderio@comcast.com>
2019-03-22 16:30:27 -0400

    multiple validation forms

--- birdboard/resources/views/projects/show.blade.php
+++ birdboard/resources/views/projects/show.blade.php
@@ -62,11 +62,17 @@
                         <button type="submit" class="button">Save</button>
                     </form>
                 </div>
+
+                @include ('errors')
             </div>
 
             <div class="lg:w-1/4 px-3">
                 @include ('projects.card')
                 @include ('projects.activity.card')
+
+                @can ('manage', $project)
+                    @include ('projects.invite')
+                @endcan
             </div>
         </div>
     </main>
